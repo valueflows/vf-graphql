e138d78d3709d240e52717b030be64e16d080977
pospi <pospi@spadgos.com>
2019-02-11 17:28:00 +1000

    export for inflated schema

---
+++ index.js
@@ -0,0 +1,22 @@
+/**
+ * Entrypoint for inflated GraphQL schema and validation helpers
+ *
+ * @package: HoloREA
+ * @author:  pospi <pospi@spadgos.com>
+ * @since:   2019-02-11
+ */
+
+const fs = require('fs')
+const { buildSchema } = require('graphql')
+
+module.exports = {
+  schema: buildSchema([
+    fs.readFileSync('schemas/config.gql'),
+    fs.readFileSync('schemas/agent.gql'),
+    fs.readFileSync('schemas/observation.gql'),
+    fs.readFileSync('schemas/planning.gql'),
+    fs.readFileSync('schemas/knowledge.gql'),
+    fs.readFileSync('schemas/query.gql'),
+    fs.readFileSync('schemas/mutation.gql'),
+  ].join('')),
+}
