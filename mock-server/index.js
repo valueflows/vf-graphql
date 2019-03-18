9159852063bc01c993d2561ae21ed54b66da3c81
pospi <pospi@spadgos.com>
2019-03-18 22:05:35 +1000

    update mock server to use built-in GraphQL mock resolvers

--- mock-server/index.js
+++ mock-server/index.js
@@ -6,16 +6,20 @@
  * @since:   2019-03-18
  */
 
-const express = require('express');
-const { ApolloServer, gql } = require('apollo-server-express');
+const express = require('express')
+const { ApolloServer, gql } = require('apollo-server-express')
+const { makeExecutableSchema, addMockFunctionsToSchema } = require('graphql-tools')
 
-const typedefs = require('@valueflows/vf-graphql').schema;
+const typeDefs = require('@valueflows/vf-graphql').typeDefs
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const schema = makeExecutableSchema({ typeDefs })
+addMockFunctionsToSchema({ schema })
 
-const app = express();
-server.applyMiddleware({ app });
+const server = new ApolloServer({ schema })
+
+const app = express()
+server.applyMiddleware({ app })
 
 app.listen({ port: 3000 }, () =>
   console.log(`🚀 Server ready at http://localhost:3000${server.graphqlPath}`)
-);
+)
